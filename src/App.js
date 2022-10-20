c08149b3519c50d1d9240bfae23646dac44af47b
Mark McNally <mark@macanalytics.ie>
2022-10-20 07:07:07 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> <i class="fa-solid fa-book-open"></i> Dictionary</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
