3f295c83d2c6db8fc6a2d0bc7f468c758c648408
Mark McNally <mark@macanalytics.ie>
2022-10-21 06:57:47 +0100

    Added search engine in dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
-        <h1> <i class="fa-solid fa-book-open"></i> Dictionary</h1>
+        <h1> <i className="fa-solid fa-book-open"></i> Dictionary</h1>
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer>
+      This project was coded by {" "} 
+      <a href="SarahClarke.io" target="_blank" rel="noopener noreferrer">
+        Sarah Clarke
+      </a>{" "}
+      and is {" "}
+      <a href="https://github.com/ClarSar/react-finalproject-dictionary-app" target="_blank" rel="noopener noreferrer">
+        Open Sourced on Github</a>
+      </footer>
+    </div>
     </div>
   );
 }
